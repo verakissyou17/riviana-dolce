ce7c114e5479f2772586320abe141e2afad885e4
verakissyou <veronicaciuca17@gmail.com>
2025-07-27 10:48:27 +0300

    wrap HashRouter in AppProviders

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import { AppProviders } from "./contexts/AppProviders"; 
+import { AppProviders } from "./contexts/AppProviders";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <AppProviders>
+    <AppProviders>
+      <HashRouter>
         <App />
-      </AppProviders>
-    </HashRouter>
+      </HashRouter>
+    </AppProviders>
   </StrictMode>
 );
